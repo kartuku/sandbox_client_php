ef645845859db66514a3950d07fb003684c94d5d
mfachri <mfachri@kartuku.co.id>
2015-12-15 17:11:57 +0700

    change ipg sandbox url

--- KartukuDirectAPI.php
+++ KartukuDirectAPI.php
@@ -6,17 +6,17 @@
  */
 class KartukuDirectAPI {
 
-    
+
     public static $MERCHANT_TOKEN = "your merchant token here";
     public static $SECRET_KEY     = "your secret key here";
-    
-    // ipgUrl    
-    const IPG_DIRECT_BASE_URL_STAGING    = "https://ipg-dev.kartuku.com/";
-    const IPG_DIRECT_BASE_URL_PRODUCTION = "https://ipg.kartuku.com/";    
-    
+
+    // ipgUrl
+    const IPG_DIRECT_BASE_URL_SANDBOX    = "https://ipg-test.kartuku.com/";
+    const IPG_DIRECT_BASE_URL_PRODUCTION = "https://ipg.kartuku.com/";
+
     // timeout in second
     private $timeOut = 10;
-    
+
     public static $production = false;
     // constant
     const COMMAND_PURCHASE = "purchase";
@@ -28,11 +28,11 @@ class KartukuDirectAPI {
     const COMMAND_VOID_PURCHASE = "voidPurchase";
     const COMMAND_REFUND = "refund";
     const COMMAND_VOID_REFUND = "voidRefund";
-    
+
     const COMMAND_TOKEN_STORE = "store";
     const COMMAND_TOKEN_REMOVE = "remove";
     const COMMAND_TOKEN_LIST = "list";
-    
+
     public function setProduction($val) {
         self::$production = $val;
     }
@@ -56,7 +56,7 @@ class KartukuDirectAPI {
     public function query($json_data) {
         return $this->wrapAndSend($json_data, $this->getTransactionUrl(self::COMMAND_QUERY));
     }
-    
+
     public function refund($json_data) {
         return $this->wrapAndSend($json_data, $this->getTransactionUrl(self::COMMAND_REFUND));
     }
@@ -64,26 +64,26 @@ class KartukuDirectAPI {
     public function voidCapture($json_data) {
         return $this->wrapAndSend($json_data, $this->getTransactionUrl(self::COMMAND_VOID_CAPTURE));
     }
-    
+
     public function voidAuthorize($json_data) {
         return $this->wrapAndSend($json_data, $this->getTransactionUrl(self::COMMAND_VOID_AUTHORIZE));
     }
-    
+
     public function voidRefund($json_data) {
         return $this->wrapAndSend($json_data, $this->getTransactionUrl(self::COMMAND_VOID_REFUND));
     }
     public function voidPurchase($json_data) {
         return $this->wrapAndSend($json_data, $this->getTransactionUrl(self::COMMAND_VOID_PURCHASE));
     }
-    
+
     public function tokenStore($json_data){
         return $this->wrapAndSend($json_data, $this->getTokenUrl(self::COMMAND_TOKEN_STORE));
     }
-    
+
     public function tokenRemove($json_data){
         return $this->wrapAndSend($json_data, $this->getTokenUrl(self::COMMAND_TOKEN_REMOVE));
     }
-    
+
     public function tokenList($json_data){
         return $this->wrapAndSend($json_data, $this->getTokenUrl(self::COMMAND_TOKEN_LIST));
     }
@@ -111,11 +111,11 @@ class KartukuDirectAPI {
             throw new Exception("Error while connecting to server", 0, $e);
         }
     }
-    
+
     private function sendMessage($url, $data){
         return $this->isCurlAvailable()? $this->sendMessageUsingCurl($url, $data) : $this->sendMessageUsingStreamContext($url, $data);
     }
-    
+
     private function wrapAndSend($json_data, $url) {
         $data = $this->transformData($json_data);
         $hash = $this->calculateHash(self::$SECRET_KEY, $data);
@@ -133,11 +133,11 @@ class KartukuDirectAPI {
     }
 
     private function getTransactionUrl($command){
-        return (self::$production ? self::IPG_DIRECT_BASE_URL_PRODUCTION : self::IPG_DIRECT_BASE_URL_STAGING) . "direct/" . $command;
+        return (self::$production ? self::IPG_DIRECT_BASE_URL_PRODUCTION : self::IPG_DIRECT_BASE_URL_SANDBOX) . "direct/" . $command;
     }
-    
+
     private function getTokenUrl($command){
-        return (self::$production ? self::IPG_DIRECT_BASE_URL_PRODUCTION : self::IPG_DIRECT_BASE_URL_STAGING) . "card/token/" . $command;
+        return (self::$production ? self::IPG_DIRECT_BASE_URL_PRODUCTION : self::IPG_DIRECT_BASE_URL_SANDBOX) . "card/token/" . $command;
     }
 
     // need testing
@@ -155,15 +155,15 @@ class KartukuDirectAPI {
 
     public function isMessageError($result) {
         $message = base64_decode($result["message"]);
-        $json = json_decode($message, true);        
+        $json = json_decode($message, true);
         return is_null($json) || (array_key_exists("ipgResponseCode", $json) && $json["ipgResponseCode"] !== "0") ;
     }
-    
-    
+
+
     public static function getIPGUrl(){
-        return self::$production ? self::IPG_DIRECT_BASE_URL_PRODUCTION : self::IPG_DIRECT_BASE_URL_STAGING;
+        return self::$production ? self::IPG_DIRECT_BASE_URL_PRODUCTION : self::IPG_DIRECT_BASE_URL_SANDBOX;
     }
-    
+
     private function isCurlAvailable() {
         if (in_array('curl', get_loaded_extensions())) {
             return true;
@@ -171,7 +171,7 @@ class KartukuDirectAPI {
             return false;
         }
     }
-    
+
     private function sendMessageUsingCurl($url, $json){
         $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
@@ -179,10 +179,10 @@ class KartukuDirectAPI {
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
         curl_setopt($ch, CURLOPT_TIMEOUT, $this->timeOut);
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $this->timeOut);        
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $this->timeOut);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
         //curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
-        //curl_setopt($ch, CURLOPT_CAINFO, getcwd() . $this->IPG_TRUSTED_CERTIFICATE);        
+        //curl_setopt($ch, CURLOPT_CAINFO, getcwd() . $this->IPG_TRUSTED_CERTIFICATE);
         $result = curl_exec($ch); // $info = curl_getinfo($ch);
         if (curl_errno($ch)) {
             throw new Exception("Connection failure : " . curl_error($ch));
@@ -190,14 +190,14 @@ class KartukuDirectAPI {
         curl_close($ch);
         return $result;
     }
-    
+
     public function setTimeOut($param) {
         $this->timeOut = $param;
     }
-    
+
     public function getTimeOut(){
         return $this->timeOut;
     }
 
-    
+
 }
